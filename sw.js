e1ebd5a18f94a7f6b65c54c4b19227ab174a6b21
trainwithZ <planslockedin@outlook.com>
2026-07-07 14:26:38 -0700

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE = "trainwith-z-v90";
+const CACHE = "trainwith-z-v91";
 const ASSETS = [
   "./",
   "./index.html",
   "./manifest.webmanifest",
-  "./src/app.js?v=68",
+  "./src/app.js?v=69",
   "./src/styles.css?v=61",
   "./src/core/analytics.js?v=1",
   "./src/core/db.js?v=3",
